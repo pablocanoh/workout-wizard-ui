8be49f3ca3f39149de659c4c97bf34b39870555b
pablocanohueros <canohueros75@gmail.com>
2023-12-23 22:05:09 +0100

    commit

--- src/login/AuthProtectedRoute.jsx
+++ src/login/AuthProtectedRoute.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { jwtDecode } from "jwt-decode";
 
 export const AuthProtectedRoute = ({ children }) => {
-    const isAuthorized = localStorage.getItem('token');
+    const token = localStorage.getItem('token');
+    let isAuthorized = false;
+
+    if (token) {
+        try {
+            // Decode the token to get its payload
+            const decodedToken = jwtDecode(token);
+            const current_time = Date.now() / 1000; // in seconds
+            if (decodedToken.exp > current_time) {
+                // Token is not expired
+                isAuthorized = true;
+            }
+        } catch (error) {
+            console.error("Token validation error:", error);
+            isAuthorized = false;
+        }
+    }
 
     if (!isAuthorized) {
-        return <Navigate to="/auth" />;
+        // Token is not present, invalid or expired
+        return <Navigate to="/auth" replace />;
     }
 
     return children;
 };
-
